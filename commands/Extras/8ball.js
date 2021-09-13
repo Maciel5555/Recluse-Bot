0ba8a74e5279b555966aeb3eefefb37bcf415f75
Maciel5555 <82708633+Maciel5555@users.noreply.github.com>
2021-09-13 06:08:11 -0200

    Update 8ball.js

--- commands/Extras/8ball.js
+++ commands/Extras/8ball.js
@@ -1,22 +1,22 @@
 const { MessageEmbed } = require('discord.js');
 const reactions = [':sweat_smile:', ':slight_smile:', ':thinking:', ':8ball:']; //Change bot's reactions
-const answers = ['Yes', 'Of Course', 'Maybe', 'No', 'Impossible', 'Why not?'];//Change bot's answers 
+const answers = ['Sim', 'Claro', 'Talvez', 'Não', 'Impossível', 'Porque não?'];//Change bot's answers 
 
 module.exports = {
   name: '8ball',
-  usage: `8ball Do you like me` ,
+  usage: `8ball Você gosta de mim?` ,
   category: "mini-games",
   aliases: ["answer", "ans"],
-  description: 'Ask the bot a Yes/No question and wait for the answer',
+  description: 'Responde sua pergunta',
   async run(client, message, args){ 
     const question = args.join(' ');
-    if (!question) return message.channel.send('**:yawning_face: - You Didn\'t Ask Me Anything .**|<:recluse7:827723090125258812> ');
+    if (!question) return message.channel.send('<:atlanta_error:736144198318686278> **Faça uma pergunta!**');
     const botans = new MessageEmbed()
       .setThumbnail('https://discord.com/assets/0cfd4882c0646d504900c90166d80cf8.svg')
       .setColor('RANDOM')
-      .setTitle(`${client.user.username}'s 8Ball Game:`)
-      .setDescription(`${message.author} Asked Me: \n\`${question}?\` \nAnd My Answer Is: \n**${reactions[Math.floor(Math.random() * reactions.length)]} - ${answers[Math.floor(Math.random() * answers.length)]} !**`)
-      .setFooter('This Game Is Made For Fun Only <:recluse5:827723187981778945> .');
+      .setTitle(`${client.user.username} 8ball:`)
+      .setDescription(`${message.author} <:help:866155256857624598> Pergunta: \n\`${question}?\` \n<:atlanta_success:736144092123234354> Resposta: \n**${reactions[Math.floor(Math.random() * reactions.length)]} - ${answers[Math.floor(Math.random() * answers.length)]} !**`)
+      .setFooter('Esse comando e so pra diversão');
       message.channel.send(botans).then(() => message.delete());
   }
 }
